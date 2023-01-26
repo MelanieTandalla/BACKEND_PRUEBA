4dfb9b75935ac04e2e9e79cfcfe9e84cfcfc55bd
Jair Delgado <jar.delgado@yavirac.edu.ec>
2023-01-26 17:57:45 -0500

    http methods for Doctor

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -44,10 +44,70 @@ const deleteDisability = async(req, res) => {
     res.json(`Discapacidad ${id} eliminada correctamente`);
 };
 
+const getDoctors = async(req, res) => {
+    const response=await pool.query('SELECT * FROM doctor');
+    console.log(response.rows);
+    res.status(200).json(response.rows);
+};
+const getDoctorsById = async(req, res) => {
+    const id=req.params.id;
+    const response =await pool.query('SELECT * FROM doctor WHERE id_doctor = $1',[id]);
+    res.json(response.rows);
+};
+const createDoctor = async(req, res) => {
+    const {nombre_d,
+        apellidos_d, 
+        email,
+        dirreccion_d, 
+        contraseña_d,
+        id_especialidades
+    }=req.body;
+   const response= await pool.query('INSERT INTO doctor (nombre_d,apellidos_d, email,dirreccion_d, contraseña_d,id_especialidades ) VALUES($1,$2,$3,$4,$5,$6)',[nombre_d,apellidos_d, email,dirreccion_d, contraseña_d,id_especialidades]);
+   console.log(response);
+    res.json({
+        message:'¡El doctor se ha creado satisfactoriamente!',
+        body:{
+            doctor: {nombre_d,apellidos_d, email,dirreccion_d, contraseña_d,id_especialidades}
+        }
+    })
+};
+const updateDoctor = async(req, res) => {
+    const id=req.params.id;
+    const { 
+        nombre_d,
+        apellidos_d, 
+        email,
+        dirreccion_d, 
+        contraseña_d,
+        id_especialidades
+    }=req.body;
+    const response = await pool.query('UPDATE doctor SET nombre_d = $1, apellidos_d = $2, email=$3,dirreccion_d = $4, contraseña_d = $5,id_especialidades=$6 WHERE id_doctor = $7',[
+        nombre_d,
+        apellidos_d, 
+        email,
+        dirreccion_d, 
+        contraseña_d,
+        id_especialidades,
+        id
+    ]) 
+    console.log(response);
+    res.json('Doctor Actualizado');
+};
+const deleteDoctor = async(req, res) => {
+    const id=req.params.id;
+    const response = await pool.query('DELETE FROM doctor WHERE id_doctor = $1',[id]);
+    res.json(`Doctor ${id} eliminado correctamente`);
+};
+
 module.exports = {
     getDisability,
-    createDisability,
     getDisabilityById,
+    getDoctors,
+    getDoctorsById,
+    createDisability,
+    createDoctor,
+    updateDisability,
+    updateDoctor,
     deleteDisability,
-    updateDisability
+    deleteDoctor
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,30 @@
 const {Router} = require('express');
 const router = Router();
 
-const {getDisability,getDisabilityById, createDisability, deleteDisability,updateDisability} = require('../controllers/index.controller')
+const {getDisability,
+    getDisabilityById,
+     createDisability, 
+     deleteDisability,
+     updateDisability,
+     getDoctors,
+     getDoctorsById,
+     createDoctor,
+     updateDoctor,
+     deleteDoctor
+} = require('../controllers/index.controller')
 
 router.get('/api/disabilities', getDisability);
+router.get('/api/doctors', getDoctors);
 router.get('/api/disabilities/:id', getDisabilityById);
-router.delete('/api/disabilities/:id', deleteDisability);
+router.get('/api/doctors/:id', getDoctorsById);
+
 router.post('/api/disabilities', createDisability);
+router.post('/api/doctors', createDoctor);
+
 router.put('/api/disabilities/:id',updateDisability);
+router.put('/api/doctors/:id',updateDoctor);
+
+router.delete('/api/disabilities/:id', deleteDisability);
+router.delete('/api/doctors/:id', deleteDoctor);
 
 module.exports = router;
